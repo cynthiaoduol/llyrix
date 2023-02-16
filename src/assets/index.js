aaeac634ab9abe7a037eb7f48a0f75fbfd43950f
cynthiaoduol <cynthiaobu940@gmail.com>
2023-02-16 11:24:44 +0300

    create the application's sidepanel

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,5 @@
-import loader from './loader.svg';
-import logo from './logo.svg';
+import loader from "./loader.svg";
+import logo from "./logo.svg";
+import llyrix from "./llyrix.svg";
 
-export {
-  logo,
-  loader,
-};
+export { logo, loader, llyrix };
